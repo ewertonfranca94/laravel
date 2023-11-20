14032b4645d5d538bc5615fa6bc6c8236467ea64
Ewerton <ewerton-1994@hotmail.com>
2023-11-20 18:30:37 -0300

    factory de usuarios funcionando

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Category;
 use App\Models\Task;
-use Database\Seeders\User;
+use App\Models\User;
+use Database\Seeders\UserSeeder;
 use Database\Seeders\CategorySeeder;
 use Illuminate\Database\Seeder;
 use database\factories\CategoryFactory;
@@ -23,6 +24,7 @@ class DatabaseSeeder extends Seeder
         ]);
         Category::factory(5)->create();
         Task::factory(30)->create();
+        User::factory(10)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
